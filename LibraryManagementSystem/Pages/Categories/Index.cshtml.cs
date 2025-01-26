4cd494ab09bdaab37513dbe60e84b4bc97735806
asudesozcu <sozcuasude@gmail.com>
2025-01-26 19:10:44 +0100

    Pagination logic added to Categories table

--- LibraryManagementSystem/Pages/Categories/Index.cshtml.cs
+++ LibraryManagementSystem/Pages/Categories/Index.cshtml.cs
@@ -7,22 +7,39 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LibraryManagementSystem.Data;
 using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LibraryManagementSystem.Pages.Categories
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly LibraryContext _context;
-
-        public List<Category> Categories { get; set; }
-        public IndexModel(LibraryContext context)
+        private readonly ILogger<IndexModel> _logger;
+       public IList<Category> Categories { get; set; } = new List<Category>();
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public IndexModel(LibraryContext context, ILogger<IndexModel> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-        public async Task OnGetAsync()
+
+        public async Task OnGetAsync(int? pageNumber)
         {
-            Categories = await _context.Categories.ToListAsync();
+            var query = _context.Categories.AsQueryable();
+
+            // Pagination logic
+            int pageSize = 10; // Number of items per page
+            CurrentPage = pageNumber ?? 1; // Default to the first page if no page is specified
+            var totalItems = await query.CountAsync(); // Total number of categories
+            TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize); // Calculate total pages
+
+            // Get the categories for the current page
+            Categories = await query.Skip((CurrentPage - 1) * pageSize)
+                                    .Take(pageSize)
+                                    .ToListAsync();
         }
     }
 }
